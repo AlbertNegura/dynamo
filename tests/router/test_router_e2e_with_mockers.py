45daf11dc6a01765313a64f4768decbb06cc9b6b
Jont828 <jt572@cornell.edu>
2026-04-02 14:08:55 -0400

    Fix pytests tests in router
    Signed-off-by: Jont828 <jt572@cornell.edu>

--- tests/router/test_router_e2e_with_mockers.py
+++ tests/router/test_router_e2e_with_mockers.py
@@ -1203,6 +1203,7 @@ def test_router_decisions_disagg(
                     frontend_port=frontend_port,
                     test_payload=TEST_PAYLOAD,
                     request_plane="nats",
+                    enable_bootstrap=enable_disagg_bootstrap,
                 )
     else:
         # Start decode workers first
@@ -1246,6 +1247,7 @@ def test_router_decisions_disagg(
                     frontend_port=frontend_port,
                     test_payload=TEST_PAYLOAD,
                     request_plane="nats",
+                    enable_bootstrap=enable_disagg_bootstrap,
                 )
 
 
